2abf6ca9908660fb544ddabebffe6fea5de521e0
Brett Gilbert <bgilb13@gmail.com>
2017-03-02 10:04:55 -0700

    added form-validate.php

---
+++ public_html/js/form-validate.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+	/* begin validation*/
+	$("#contactForm").validate({
+
+		// setup handling of form errors
+		debug: true,
+		errorClass: "alert alert-danger",
+		errorLabelContainer: "#output-area",
+		errorElement: "div",
+
+		// rules define what is good/bad input
+		// each rule starts with the form input element's NAME attribute
+		rules: {
+			name: {
+				required: true
+			},
+			email: {
+				email: true,
+				required: true
+			},
+			subject: {
+				required: true
+			},
+			message: {
+				required: true,
+			}
+		},
+
+		// error messages to display to the end user
+		messages: {
+			name: {
+				required: "Please enter your name."
+			},
+			email: {
+				email: "Please enter a valid email address.",
+				required: "Please enter a valid email address."
+			},
+			subject: {
+				required: "Please enter a subject."
+			},
+			message: {
+				required: "Please enter a message."
+			}
+		},
+
+		submitHandler: function(form) {
+			$("#contactForm").ajaxSubmit({
+				type: "POST",
+				url: $("#contactForm").attr("action"),
+
+				success: function(ajaxOutput) {
+					// clear the output area's formatting
+					$("#output-area").css("display", "");
+
+					// write the server's reply to the output area
+					$("#output-area").html(ajaxOutput);
+
+					// reset the form if it was successful
+					if($(".alert-success").length >= 1) {
+						$("#contactForm")[0].reset();
+					}
+				}
+			})
+		}
+
+	});/* end validate function */
+
+});/*end document.ready()*/
